36bc220cb5a877e1d1916cca9532cd8800a89860
chameleon <chameleon.rising@gmail.com>
2013-05-16 23:16:06 +0700

    First GMap
    First GMap

---
+++ src/com/myandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_fragment);
+    }
+}
